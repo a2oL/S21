70d425e1c53c87dad1e68d4cda01aec0ff778b85
a2oL <66704377+a2oL@users.noreply.github.com>
2020-07-26 11:57:54 -0500

    1P

--- src/main/java/com/example/s2/s2/app/ServicioS2.java
+++ src/main/java/com/example/s2/s2/app/ServicioS2.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages={"com.example.s2.s2.controller","com.example.s2.s2.repositories","com.example.s2.s2.models","com.example.s2.s2.interfaces","com.example.s2.s2.app","com.example.s2.s2.mapers"})
-public class DemoApplication {
+public class ServicioS2 {
 
     public static void main(final String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(ServicioS2.class, args);
     }
 
 }
