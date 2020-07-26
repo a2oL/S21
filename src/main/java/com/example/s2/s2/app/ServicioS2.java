15319face27cd35bd7fe470bfbd9582252b085c9
a2oL <66704377+a2oL@users.noreply.github.com>
2020-07-26 12:05:01 -0500

    dircc

--- src/main/java/com/example/s2/s2/app/ServicioS2.java
+++ src/main/java/com/example/s2/s2/app/ServicioS2.java
@@ -3,7 +3,7 @@ package com.example.s2.s2.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages={"com.example.s2.s2.controller","com.example.s2.s2.repositories","com.example.s2.s2.models","com.example.s2.s2.interfaces","com.example.s2.s2.app","com.example.s2.s2.mapers"})
+@SpringBootApplication(scanBasePackages={"com.example.s2.s2.controller","com.example.s2.s2.repositories","com.example.s2.s2.models","com.example.s2.s2.interfaces","com.example.s2.s2.app","com.example.s2.s2.mappers"})
 public class ServicioS2 {
 
     public static void main(final String[] args) {
--- src/main/java/com/example/s2/s2/controller/Ts2ServidorPublicoController.java
+++ src/main/java/com/example/s2/s2/controller/Ts2ServidorPublicoController.java
@@ -8,16 +8,16 @@ import com.example.s2.s2.models.Ts2ServidorPublico;
 import com.example.s2.s2.repositories.Ts2ServidorPublicoDAOREP;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/Ts2ServidorPublico")
+//@RequestMapping("/Ts2ServidorPublico")
 public class Ts2ServidorPublicoController {
     @Resource 
     Ts2ServidorPublicoDAOREP ts2ServidorPublicoDAOREP;
     
-    @GetMapping(value = "/Ts2ServidorPublicoList")
+    @GetMapping(value = "/datos")
     public List<Ts2ServidorPublico> getTs2ServidorPublico() {
         return ts2ServidorPublicoDAOREP.findAll();
     }
